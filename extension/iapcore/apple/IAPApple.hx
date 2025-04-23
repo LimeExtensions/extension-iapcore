c00fa0a4955109b8b0a244bcfa09ca183617ce02
MAJigsaw77 <77043862+MAJigsaw77@users.noreply.github.com>
2025-04-24 02:15:34 +0300

    AAA

--- extension/iapcore/apple/IAPApple.hx
+++ extension/iapcore/apple/IAPApple.hx
@@ -30,7 +30,7 @@ class IAPApple
 	{
 		if (productIdentifiers != null && productIdentifiers.length > 0)
 		{
-			final ptr:cpp.RawPointer<cpp.ConstCharStar> = cast cpp.Stdlib.nativeMalloc(productIdentifiers.length * cpp.Stdlib.sizeof(untyped __cpp__('const char *')));
+			final ptr:cpp.RawPointer<cpp.ConstCharStar> = cast cpp.Stdlib.nativeMalloc(productIdentifiers.length * untyped __cpp__('sizeof(const char *)'));
 
 			for (i in 0...productIdentifiers.length)
 				ptr[i] = cpp.ConstCharStar.fromString(productIdentifiers[i]);
